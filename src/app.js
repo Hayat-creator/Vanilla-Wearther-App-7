4a3fa9b1424c2425d1876d06ddd4ba7c99218924
Hayat-creator <lovellyayaan4ever@gmail.com>
2023-09-24 01:08:09 +0200

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElemen = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElemen.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
 
+let apiKey = "2cc35b89df9876cecdab04e7819f9629";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Innsbruck&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
