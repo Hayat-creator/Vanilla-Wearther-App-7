8802c71159fd33650ca8cb8889ed5d653742df95
Hayat-creator <lovellyayaan4ever@gmail.com>
2023-09-25 20:21:29 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -43,8 +43,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "2cc35b89df9876cecdab04e7819f9629";
-let city = "Innsbruck";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "2cc35b89df9876cecdab04e7819f9629";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+search("Innsbruck");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
